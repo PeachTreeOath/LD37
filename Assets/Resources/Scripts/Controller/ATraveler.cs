f949d927ec9b5270c905a852da79030f9d53f8ab
Makenna Fish <makenna.g.f@gmail.com>
2016-12-10 00:11:21 -0700

    Working random travel behavior.

---
+++ Assets/Resources/Scripts/Controller/ATraveler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Controller
+{
+    public class ATraveler : MonoBehaviour
+    {
+        protected DIRECTION2D direction2D;
+
+        /// <summary>
+        /// Private random generator.
+        /// </summary>
+        protected System.Random random = new System.Random();
+
+        protected float xMultiplier = 1;
+        protected float yMultiplier = 1;
+
+        protected enum DIRECTION2D
+        {
+            UP,
+            DOWN,
+            LEFT,
+            RIGHT
+        }
+
+        /// <summary>
+        /// Randomly sets the 2D direction
+        /// </summary>
+        protected void RandomizeDirection2D()
+        {
+            direction2D = (DIRECTION2D)random.Next(1, 5);
+        }
+    }
+}
--- Assets/Resources/Scripts/Controller/Moving Obstacles/RandomTravelBehavior.cs
+++ Assets/Resources/Scripts/Controller/Moving Obstacles/RandomTravelBehavior.cs
@@ -1,16 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class RandomTravelBehavior : MonoBehaviour {
+namespace Controller
+{
+    /// <summary>
+    /// Supports driving a model in random direction across the screen. Uses the
+    /// turn probability level to determine how frequently the model randomly
+    /// changes it's direction.
+    /// </summary>
+    public class RandomTravelBehavior : ATraveler
+    {
+        /// <summary>
+        /// The probability at which a model will roll for a different direction.
+        /// Represented as a value from 1-100. The larger the number, the greater
+        /// the chance of the model turning.
+        /// </summary>
+        public double turnProbability = 10;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        /// <summary>
+        /// The rate at which
+        /// </summary>
+        public float speed = 0.2f;
+
+        /// <summary>
+        /// Container for random movement value.
+        /// </summary>
+        private double curVal = 0;
+
+        // Update is called once per frame
+        private void Update()
+        {
+            curVal = random.Next(1, 101);
+            if (curVal >= turnProbability)
+            {
+                switch (direction2D)
+                {
+                    case DIRECTION2D.UP:
+                        transform.position = new Vector3(transform.position.x * speed, transform.position.y, transform.position.z);
+                        break;
+
+                    case DIRECTION2D.DOWN:
+                        transform.position = new Vector3(transform.position.x * speed * -1, transform.position.y, transform.position.z);
+                        break;
+
+                    case DIRECTION2D.LEFT:
+                        transform.position = new Vector3(transform.position.x, transform.position.y * speed, transform.position.z);
+                        break;
+
+                    case DIRECTION2D.RIGHT:
+                        transform.position = new Vector3(transform.position.x, transform.position.y * speed * -1, transform.position.z);
+                        break;
+                }
+            }
+        }
+    }
 }
