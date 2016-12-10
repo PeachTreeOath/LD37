0bc1ff9d7a82b2ca3f1a05868ca857bcc525b105
azuark <tomrogers917@gmail.com>
2016-12-10 01:11:07 -0700

    Persistant scene

--- Assets/Resources/Scripts/UpgradeButton.cs
+++ Assets/Resources/Scripts/UpgradeButton.cs
@@ -10,16 +10,67 @@ public class UpgradeButton : MonoBehaviour {
     public Text Money;
     public Text Cost;
     public Text Description;
+    private UpgradeManager upManager;
     
+    public void Start()
+    {
+        GameObject manager = GameObject.FindGameObjectWithTag("UpManage");
+        upManager = manager.GetComponent<UpgradeManager>();
+    }
     public void Upgrade()
     {
+        Button button = this.gameObject.GetComponent<Button>();
         int value = int.Parse(ButtonText.text);
         int moneyValue = int.Parse(Money.text);
         if(value < MaxLevel)
         {
             value++;
+            UpdateManager(value);
+        }
+        if(value == MaxLevel)
+        {
+            button.enabled = false;
+            Text text = this.GetComponentInChildren<Text>();
+            if(text != null)
+            {
+                text.text = "MAXED";
+            }
+            
         }
         ButtonText.text = value.ToString();
+        
+    }
+
+    public void UpdateManager(int value)
+    {
+        string name = this.gameObject.name;
+        switch(name)
+        {
+            case "Knife Button":
+                upManager.thornLevel = value;
+                break;
+            case "Eagle Eye Button":
+                upManager.visionLevel = value;
+                break;
+            case "Muscle Mix of Mixing":
+                upManager.energyLevel = value;
+                break;
+            case "Max Gainz":
+                upManager.deepCleanLevel = value;
+                break;
+            case "AoE Cleaning":
+                upManager.cleanRadiusLevel = value;
+                break;
+            case "Rhonda's Legs of Running":
+                upManager.speedLevel = value;
+                break;
+            case "Rhonda's Rockets of Launching":
+                upManager.launchSpeedLevel = value;
+                break;
+            case "Rhonda's Omnidirectional Turning Ability":
+                upManager.turningLevel = value;
+                break;
+        }
     }
 
 }
---
+++ Assets/UpgradeManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour {
+
+    [HideInInspector]
+    public int thornLevel;
+    [HideInInspector]
+    public int visionLevel;
+    [HideInInspector]
+    public int energyLevel;
+    [HideInInspector]
+    public int deepCleanLevel;
+    [HideInInspector]
+    public int cleanRadiusLevel;
+    [HideInInspector]
+    public int speedLevel;
+    [HideInInspector]
+    public int launchSpeedLevel;
+    [HideInInspector]
+    public int turningLevel;
+    [HideInInspector]
+
+
+    UpgradeManager instance = null;
+	// Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+            instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
