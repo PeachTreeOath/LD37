1027b442efc7d75bc53eb2e8f48c490ef726a042
PeachTreeOath <peachtreeoath@gmail.com>
2016-12-10 01:19:50 -0700

    Janky arrow code

--- Assets/Resources/Scripts/ArrowPointer.cs
+++ Assets/Resources/Scripts/ArrowPointer.cs
@@ -19,6 +19,35 @@ public class ArrowPointer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (CheckIfOnScreen(roombaBase))
+        {
+            float bounceVal = Mathf.Abs(Mathf.Sin(Time.time * 3) / 3);
+            float offset = 1.5f;
+            arrow.transform.position = roombaBase.transform.position;
+            arrow.transform.up = roombaBase.transform.position - (arrow.transform.position + arrow.transform.localPosition);
+            arrow.transform.localPosition = new Vector2(0, bounceVal + offset);
+            arrow.transform.localScale = new Vector2(1.5f, -1.5f);
+        }
+        else
+        {
+            Vector3 llBounds = Camera.main.ViewportToWorldPoint(new Vector3(0.1f, 0.1f));
+            Vector3 urBounds = Camera.main.ViewportToWorldPoint(new Vector3(0.95f, 0.95f));
 
+            arrow.transform.position = new Vector2(Mathf.Clamp(roombaBase.transform.position.x, llBounds.x, urBounds.x), Mathf.Clamp(roombaBase.transform.position.y, llBounds.y, urBounds.y));
+            Quaternion rotation = Quaternion.LookRotation(roombaBase.transform.position - arrow.transform.position, arrow.transform.TransformDirection(Vector3.back));
+            arrow.transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+            arrow.transform.localScale = new Vector2(1.5f, 1.5f);
+        }
+    }
+
+    private bool CheckIfOnScreen(GameObject obj)
+    {
+        Vector2 point = Camera.main.WorldToViewportPoint(obj.transform.position);
+
+        if (point.x > 0 && point.y > 0 && point.x < 1 && point.y < 1)
+        {
+            return true;
+        }
+        return false;
     }
 }
