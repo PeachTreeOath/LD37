910a075e52be377879c22d5f08538524914a30d3
azuark <tomrogers917@gmail.com>
2016-12-10 00:26:36 -0700

    Some Button Logic

---
+++ Assets/Resources/Scripts/UpgradeButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeButton : MonoBehaviour {
+
+    public Text ButtonText;
+    public int MaxLevel;
+    public Text Money;
+    public Text Cost;
+    public Text Description;
+    
+    public void Upgrade()
+    {
+        int value = int.Parse(ButtonText.text);
+        int moneyValue = int.Parse(Money.text);
+        if(value < MaxLevel)
+        {
+            value++;
+        }
+        ButtonText.text = value.ToString();
+    }
+
+}
