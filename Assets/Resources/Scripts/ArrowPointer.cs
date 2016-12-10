7247690b6ecc04e125d1f651bbf7cdcf8a0174bb
PeachTreeOath <peachtreeoath@gmail.com>
2016-12-10 00:47:15 -0700

    Base arrow

---
+++ Assets/Resources/Scripts/ArrowPointer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowPointer : MonoBehaviour
+{
+
+    public GameObject roombaBase;
+
+    private GameObject arrow;
+
+    // Use this for initialization
+    void Start()
+    {
+        arrow = Instantiate(Resources.Load<GameObject>("Prefabs/BaseArrow"));
+        arrow.transform.position = roombaBase.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
