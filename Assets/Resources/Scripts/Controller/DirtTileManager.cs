31923d3cd4e19e778c6d0033f73acb088f6cd562
Gary Wong <gawong@microsoft.com>
2016-12-10 14:54:15 -0800

    Update dirt

--- Assets/Resources/Scripts/Controller/DirtTileManager.cs
+++ Assets/Resources/Scripts/Controller/DirtTileManager.cs
@@ -10,41 +10,41 @@ public class DirtTileManager : MonoBehaviour
     /// <summary>
     /// The current opacity value of the tile, as a percentage.
     /// </summary>
-    public float opacityPercentage = 0.7f;
+    public float opacityPercentage;
 
     // Use this for initialization
-    private void Start()
-    {
+    private void Start() {
         curColor = GetComponent<SpriteRenderer>().color;
+        curColor.a = 1;
+        opacityPercentage = 0.7f;
         UpdateOpacity();
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log("Entered dirt OnTriggerEnter2D.");
-
+    private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
-            Debug.Log("Vacuum collided with dirt.");
+            int curUpgrade = UpgradeManager.Instance.GetUpgradeValue(UpgradeManager.UpgradeEnum.DEEP_CLEAN) + 1;
+
+            // Max Deep Clean upgrade level is 5.
+            if (curUpgrade > 5)
+                curUpgrade = 5;
+
+            // Decrement the opacity value by the amount afforded by the current
+            // upgrade level. Level 0 decrements by 1/5, Level 1 by 2/5, etc
+            opacityPercentage = opacityPercentage - opacityPercentage * (curUpgrade / 5.0f);
+            Debug.Log("Opacity " + opacityPercentage.ToString());
+            if (opacityPercentage <= 0f) {
+                Destroy(this.gameObject);
+            }
+            UpdateOpacity();
         }
-
-        int curUpgrade = UpgradeManager.Instance.GetUpgradeValue(UpgradeManager.UpgradeEnum.DEEP_CLEAN) + 1;
-
-        // Max Deep Clean upgrade level is 5.
-        if (curUpgrade > 5)
-            curUpgrade = 5;
-
-        // Decrement the opacity value by the amount afforded by the current
-        // upgrade level. Level 0 decrements by 1/5, Level 1 by 2/5, etc
-        opacityPercentage = opacityPercentage - (curUpgrade / 5);
-        UpdateOpacity();
     }
 
     /// <summary>
     /// Update the opacity of the sprite renderer to reflect the current opacity
     /// percentage value.
     /// </summary>
-    private void UpdateOpacity()
-    {
-        curColor = new Color(curColor.r, curColor.g, curColor.b, 255 * opacityPercentage);
+    private void UpdateOpacity() {
+        curColor = new Color(curColor.r, curColor.g, curColor.b, curColor.a * opacityPercentage);
+        GetComponent<SpriteRenderer>().color = curColor;
     }
 }
