022574524606c5cd5176a9665605392309c50466
PeachTreeOath <peachtreeoath@gmail.com>
2016-12-10 03:49:41 -0700

    File clean up

---
+++ Assets/Resources/Scripts/RoombaData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoombaData : MonoBehaviour {
+
+    public int health;
+    public float batteryLife;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// TODO: Hook this up to UI
+	}
+}
