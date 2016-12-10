2a46b2da1fd096b9a9b6a3f92d427464ff378f25
PeachTreeOath <peachtreeoath@gmail.com>
2016-12-09 23:10:12 -0700

    Comin in hot

---
+++ Assets/Resources/Scripts/BackgroundLoader.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Takes an image and creates a grid out of it.
+public class BackgroundLoader : MonoBehaviour
+{
+
+    public int rowNum;
+    public int colNum;
+
+    // Background prefab. A prefab made up of 1 sprite is sufficient.
+    public GameObject bgPrefab;
+    // Parent object in hierarchy to create bg objects under.
+    public GameObject parentGameObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        CreateBG();
+    }
+
+    private void CreateBG()
+    {
+        Vector2 size = bgPrefab.GetComponent<SpriteRenderer>().bounds.size;
+
+        float startX = (size.x * colNum * -.5f) + size.x / 2;
+        float startY = (size.y * rowNum * .5f) - size.y / 2;
+        float currX = startX;
+        float currY = startY;
+
+        for (int i = 0; i < rowNum; i++)
+        {
+            for (int j = 0; j < colNum; j++)
+            {
+                GameObject bg = ((GameObject)Instantiate(bgPrefab, Vector2.zero, Quaternion.identity));
+                bg.transform.position = new Vector2(currX, currY);
+                bg.transform.SetParent(parentGameObject.transform);
+                currX += size.x;
+            }
+            currY -= size.y;
+            currX = startX;
+        }
+    }
+}
