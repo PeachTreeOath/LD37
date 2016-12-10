a1c64bed9039d0675666f64ee6ada04163da2a71
PeachTreeOath <peachtreeoath@gmail.com>
2016-12-10 15:06:48 -0700

    Early audio mgr push

--- Assets/Resources/Scripts/AudioManager.cs
+++ Assets/Resources/Scripts/AudioManager.cs
@@ -4,16 +4,48 @@ using UnityEngine;
 
 public class AudioManager : Singleton<AudioManager> {
 
+    public enum SoundClip
+    {
+        BOUNCE,
+        BUG,
+        BUZZ,
+        COLLISION_SOFA,
+        COLLISION_WOOD,
+        CONSTANT,
+        CRUMB,
+        DOG,
+        GENERIC_SHOOT_HIGH,
+        GENERIC_SHOOT_MID,
+        KITTY,
+        LOW_BATTERY,
+        MONEY_BUY,
+        MONEY_INVALID,
+        PICKUP_SOUND,
+        PRESSING_START,
+        ROCKETS,
+        ROOMBA,
+        ROOMBA_OVER_CARPET,
+        SELECT
+    }
+
     private AudioSource musicChannel;
     private AudioSource soundChannel;
+    private AudioClip[] clips;
 
     // Use this for initialization
     void Start () {
-        musicChannel = gameObject.AddComponent<AudioSource>();
-	}
+        musicChannel = Instantiate(Resources.Load<GameObject>("Prefabs/AudioChannel")).GetComponent<AudioSource>();
+        musicChannel.transform.SetParent(transform);
+        musicChannel.loop = true;
+        soundChannel = Instantiate(Resources.Load<GameObject>("Prefabs/AudioChannel")).GetComponent<AudioSource>();
+        soundChannel.transform.SetParent(transform);
+
+        clips = Resources.LoadAll<AudioClip>("Audio");
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void PlaySound(string name)
+    {
+
+    }
+
 }
