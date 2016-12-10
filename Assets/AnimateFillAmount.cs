8a625aa4a107f35830a4bd31e54796ec5f521d6b
PeachTreeOath <peachtreeoath@gmail.com>
2016-12-10 03:40:45 -0700

    Percentage code added to UI

--- Assets/AnimateFillAmount.cs
+++ Assets/AnimateFillAmount.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AnimateFillAmount : MonoBehaviour {
+public class AnimateFillAmount : MonoBehaviour
+{
     public float timeLeft = 1;
     public Image image;
     public bool coolingDown;
     public float waitTime = 30.0f;
+    private Text text;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         image = GetComponent<Image>();
+        text = GetComponentInChildren<Text>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         //amount -= 0.1f;
         image.fillAmount -= 1.0f / waitTime * Time.deltaTime;
+            text.text = (int)(image.fillAmount * 100) + "%";
         if (coolingDown == true)
         {
             //Reduce fill amount over 30 seconds
