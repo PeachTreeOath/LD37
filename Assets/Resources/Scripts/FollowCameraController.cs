e067a3aff750f07c75c2a94cf6d6169734ad5650
Gary Wong <gawong@microsoft.com>
2016-12-10 00:20:40 -0800

    Follow camera not working

---
+++ Assets/Resources/Scripts/FollowCameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        // Get distance from roomba starting position
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+	}
+}
