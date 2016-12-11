d450a6e4419aa2fee22d51e753e48df039493392
PeachTreeOath <peachtreeoath@gmail.com>
2016-12-11 01:01:03 -0700

    Load persist scene if you dont have it loaded

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -1,17 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
 
-	// Use this for initialization
-	void Start () {
+    public string persistSceneName = "PersistentUpgrades";
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        bool isPersistenceSceneLoaded = false;
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+            if (scene.name.Equals(persistSceneName))
+            {
+                isPersistenceSceneLoaded = true;
+                break;
+            }
+        }
+
+        if (!isPersistenceSceneLoaded)
+        {
+            SceneManager.LoadScene(persistSceneName, LoadSceneMode.Additive);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
         AudioManager.instance.PlayRoomMusic(.25f);
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
