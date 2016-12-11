a7a5a3789a5e0a77bec3b81f946a71c70d9e6425
Makenna Fish <makenna.g.f@gmail.com>
2016-12-10 18:13:29 -0700

    Added collision sounds to couch and walls

---
+++ Assets/Resources/Scripts/Controller/Stationary Obstacles/WallController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+internal class WallController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            AudioManager.instance.PlaySound("collision_wood");
+        }
+    }
+}
