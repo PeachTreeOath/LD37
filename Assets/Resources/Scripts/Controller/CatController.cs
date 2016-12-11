1908af24d0da4cde1a4564f2fb50c8d70b1717eb
PeachTreeOath <peachtreeoath@gmail.com>
2016-12-10 18:06:10 -0700

    Bounce sounds

--- Assets/Resources/Scripts/Controller/CatController.cs
+++ Assets/Resources/Scripts/Controller/CatController.cs
@@ -5,9 +5,4 @@ using UnityEngine;
 public class CatController : MonoBehaviour
 {
     
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        AudioManager.instance.PlaySound("kitty");
-        AudioManager.instance.PlaySound("Bounce");
-    }
 }
---
+++ Assets/Resources/Scripts/SofaSoundPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SofaSoundPlayer : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        AudioManager.instance.PlaySound("collision_sofa");
+        AudioManager.instance.PlaySound("Bounce");
+    }
+}
