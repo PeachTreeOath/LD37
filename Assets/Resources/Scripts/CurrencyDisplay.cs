247928a6542a0230425eb03655cc26f7e3a6ba1e
Makenna Fish <makenna.g.f@gmail.com>
2016-12-11 02:51:59 -0700

    Total currency updates in upgrades panel.

---
+++ Assets/Resources/Scripts/CurrencyDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencyDisplay : MonoBehaviour
+{
+    public Text textbox;
+
+    // Use this for initialization
+    private void Start()
+    {
+        if (textbox == null)
+            textbox = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        textbox.text = UpgradeManager.instance.money.ToString();
+    }
+}
