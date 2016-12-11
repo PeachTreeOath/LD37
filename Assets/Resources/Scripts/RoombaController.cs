254f5d72e9be3b64935779e7f3927c0ec7341a63
Makenna Fish <makenna.g.f@gmail.com>
2016-12-10 22:59:39 -0700

    Adding docking.

--- Assets/Resources/Scripts/RoombaController.cs
+++ Assets/Resources/Scripts/RoombaController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoombaController : MonoBehaviour {
+public class RoombaController : MonoBehaviour
+{
     private Rigidbody2D rb;
     private RoombaData rd;
     public float rotUpgradeMult;
@@ -20,6 +21,8 @@ public class RoombaController : MonoBehaviour {
     /// </summary>
     private float currReverseTime = 0;
 
+    private float dockTime = 0;
+
     /// <summary>
     /// Adjusts roomba flyback speed.
     /// </summary>
@@ -32,10 +35,23 @@ public class RoombaController : MonoBehaviour {
 
     private bool isBraking;
 
+    /// <summary>
+    /// Flag indicating if the roomba is docking.
+    /// </summary>
+    private bool isDocking;
+
+    /// <summary>
+    /// The time it takes to dock in ms.
+    /// </summary>
+    private const float DOCK_TIME = 1000;
+
+    private Vector3 dockLocation;
+
     private float dragControl;
 
     // Use this for initialization
-    private void Start() {
+    private void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         rd = GetComponent<RoombaData>();
         lastPos = gameObject.transform.position;
@@ -44,52 +60,80 @@ public class RoombaController : MonoBehaviour {
     }
 
     // Update is called once per frame
-    private void Update() {
+    private void Update()
+    {
         transform.FindChild("RoombaBody").rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0, 0);
+
         isBraking = Input.GetKey(KeyCode.Space) | Input.GetKey(KeyCode.S) | Input.GetKey(KeyCode.DownArrow);
-        if (isBraking) {
+        if (isBraking)
+        {
             rb.drag = Mathf.Lerp(0, 10, dragControl);
-            if (dragControl < 1) {
+            if (dragControl < 1)
+            {
                 dragControl += Time.deltaTime / 5f;
             }
         }
-
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         float moveHorizontal = Input.GetAxis("Horizontal");
 
         if (isReversing)
             rb.AddForce(transform.up * Mathf.Lerp(rd.maxMoveSpeed + bounceVelocity, rd.minMoveSpeed, (currReverseTime / reverseDuration)));
-        else {
+        else if (isDocking)
+        {
+            dockTime += Time.deltaTime * 1000;
+            // After a set amount of time has passed, jump to the docked position.
+            if (dockTime >= DOCK_TIME)
+            {
+                transform.localPosition = new Vector3(dockLocation.x, dockLocation.y - 0.11f);
+                Time.timeScale = 0;
+                AudioManager.instance.PlaySound("Buzz");
+            }
+            else
+            {
+                // Before the time limit has passed, attempt to lerp to the dock.
+                dockTime += Time.deltaTime * 1000;
+                transform.localPosition = Vector2.Lerp(transform.localPosition, new Vector3(dockLocation.x, dockLocation.y - 0.11f), dockTime / DOCK_TIME);
+            }
+        }
+        else
+        {
             float minSpeed = rd.minMoveSpeed + UpgradeManager.Instance.GetUpgradeValue(UpgradeManager.UpgradeEnum.SPEED) * 5;
             float maxSpeed = rd.maxMoveSpeed + UpgradeManager.Instance.GetUpgradeValue(UpgradeManager.UpgradeEnum.SPEED) * 2;
             float force = -Mathf.Lerp(minSpeed, maxSpeed, (Time.time - startTime) * rd.accelSpeed);
 
             rb.AddForce(transform.up * force);
 
-            if (isBraking) {
+            if (isBraking)
+            {
                 rb.AddForce(transform.up * -force);
-            } else {
+            }
+            else
+            {
                 rb.drag = 10;
                 dragControl = 0;
             }
-
-
         }
 
         float rSpeed = rd.rotSpeed + UpgradeManager.Instance.GetUpgradeValue(UpgradeManager.UpgradeEnum.TURN_RADIUS) * rotUpgradeMult;
-        if (Vector3.Distance(gameObject.transform.position, lastPos) > 0) {
+        if (Vector3.Distance(gameObject.transform.position, lastPos) > 0)
+        {
             rb.MoveRotation(rb.rotation - moveHorizontal * rSpeed);
-        } else {
+        }
+        else
+        {
             rb.MoveRotation(rb.rotation + moveHorizontal * rSpeed);
         }
         lastPos = gameObject.transform.position;
 
-        if (isReversing) {
+        if (isReversing)
+        {
             dragControl = 1;
             currReverseTime += Time.deltaTime * 1000;
-            if (currReverseTime >= reverseDuration) {
+            if (currReverseTime >= reverseDuration)
+            {
                 currReverseTime = 0;
                 isReversing = false;
                 startTime = Time.time;
@@ -97,17 +141,26 @@ public class RoombaController : MonoBehaviour {
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         // Trigger a reverse after collision with obstacle
-        if (other.gameObject.CompareTag("Obstacle")) {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
             bounceVelocity = 2;
             reverseDuration = 500;
             isReversing = true;
-        } else if (other.gameObject.CompareTag("AnimalObstacle")) {
+        }
+        else if (other.gameObject.CompareTag("AnimalObstacle"))
+        {
             bounceVelocity = 7;
             reverseDuration = 700;
             isReversing = true;
-			GetComponent<BatteryController>().Damage();
+            GetComponent<BatteryController>().Damage();
+        }
+        else if (other.gameObject.CompareTag("Dock"))
+        {
+            isDocking = true;
+            dockLocation = other.transform.localPosition;
         }
     }
 }
