30522b05afc32f7b83f1f72840d58af0511b5f67
Makenna Fish <makenna.g.f@gmail.com>
2016-12-10 19:18:17 -0700

    Created empty main-menu which loads game

---
+++ Assets/Resources/Scripts/Controller/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    /// <summary>
+    /// Starts the game
+    /// </summary>
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+}
