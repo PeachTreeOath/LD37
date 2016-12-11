fbda4033d6e577b662d0365372de11caca422163
Fangor <rfonger@gmail.com>
2016-12-10 23:20:33 -0700

    Fixing shop music

--- Assets/Resources/Scripts/ShopMusicLoader.cs
+++ Assets/Resources/Scripts/ShopMusicLoader.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : Singleton<GameManager>
-{
+public class ShopMusicLoader : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
         AudioManager audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
-        audioManager.PlayRoomMusic(.25f);
+        audioManager.PlayShopMusic(.25f);
     }
 	
 	// Update is called once per frame
