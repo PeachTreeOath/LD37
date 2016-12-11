f741de84e19abc80e9d837c736be6031613df24b
Thundercleez <tmizas@gmail.com>
2016-12-11 14:00:14 -0500

    Added money loss when battery runs out.

--- Assets/Resources/Scripts/UpgradeManager.cs
+++ Assets/Resources/Scripts/UpgradeManager.cs
@@ -8,7 +8,7 @@ public class UpgradeManager : MonoBehaviour {
 
 	static Dictionary<UpgradeEnum, Upgrade> upgrades;
 
-    public static int money = 200;
+    public static int money = 0;
 
 	static UpgradeManager instance;
 
